632d917b8fbd228c2975de095ca3411a4d064697
marystefia <135801243+marystefia@users.noreply.github.com>
2023-09-25 22:25:32 +0530

    Delete Week1 directory

--- Week1/EdgeBrowser.java
+++
@@ -1,9 +0,0 @@
-package week1.day2;
-
-public class EdgeBrowser{
-	public static void main(String[] args) {
-		Browser obj=new Browser();
-		System.out.println(obj.launchBrowser("Edge")+" browser loaded successfully");
-		obj.loadUrl();
-	}
-}
